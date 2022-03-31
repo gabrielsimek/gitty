6816a8d90be43e6e993168b59a06fcfaf439c473
Gabriel Simek <gabrielsimek@outlook.com>
2022-03-31 00:27:09 -0700

    add post post

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,17 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
 
 CREATE TABLE users(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    username TEXT NOT NULL,
+    username TEXT NOT NULL UNIQUE,
     email TEXT,
     avatar TEXT
-)
+);
 
 CREATE TABLE posts(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     post VARCHAR(255),
     username TEXT REFERENCES users(username)
-)
+);
