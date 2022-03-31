a313ef0640dc29d6b05270b57a6ab66e8ed5f4ea
Gabriel Simek <gabrielsimek@outlook.com>
2022-03-31 00:42:34 -0700

    getall

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -52,10 +52,43 @@ describe.only('posts routes', () => {
     await agent.get('/api/v1/github/login/callback?code=123').redirects(1);
 
     res = await agent.post('/api/v1/posts').send(mockPost);
-    
+
     expect(res.body).toEqual({
       ...mockPost,
       id: expect.any(String),
     });
+
+    res = await agent
+      .post('/api/v1/posts')
+      .send({ post: Array(256).fill('a').join('') });
+
+    expect(res.status).not.toBe(200);
+    expect(res.body.message).toEqual('Posts must be less than 255 characters');
+  });
+
+  it('allows authenicated users to view all posts', async () => {
+    const agent = request.agent(app);
+
+    const mockPost = { post: 'example post', username: 'fake_github_user' };
+
+    let res = await agent.get('/api/v1/posts');
+
+    expect(res.status).toBe(401);
+    expect(res.body.message).toEqual('You must be logged in to continue');
+
+    await agent.get('/api/v1/github/login/callback?code=123').redirects(1);
+
+    const posts = await Promise.all(
+      [...Array(3)].map(async (_, i) => {
+        const { body } = await agent
+          .post('/api/v1/posts')
+          .send({ ...mockPost, post: mockPost.post + i });
+
+        return body;
+      })
+    );
+
+    res = await agent.get('/api/v1/posts');
+    expect(res.body).toEqual(expect.arrayContaining(posts));
   });
 });
